9d369f2e7eb6d4a10e78be4fad6321bb1d534b07
Jaroon Umpornpoolsuk <jaroon_ump_fth@tdem.toyota-asia.com>
2024-03-24 18:14:53 +0700

    lab01-question-02v2

--- src/test/unit-test/utils/userManager.test.ts
+++ src/test/unit-test/utils/userManager.test.ts
@@ -2,22 +2,32 @@ import { findUserById, checkUserEmail, findNameInArray } from '../../../app/util
 
 describe('User Manager Tests', () => {
   it('should return a user object when found by ID', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // const user = findUserById(1);
+
+    // // ตรวจสอบว่าผลลัพธ์ตรงกับ object ที่คาดหวัง
+    // expect(xxxxx).xxxxx({ id: 1, name: "John Doe", email: "john@example.com" });
   });
 
   it('should return undefined for a user not found by ID', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // const user = findUserById(99);
+    // expect(xxxxx).xxxxx(); // ตรวจสอบว่าผลลัพธ์เป็น undefined สำหรับผู้ใช้ที่ไม่พบ
   });
 
   it('should confirm a user has an email defined', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // const user = findUserById(1);
+    // expect(checkUserEmail(user!)).xxxxx(); // ตรวจสอบว่าผู้ใช้นี้มีอีเมล
   });
 
   it('should confirm a user has no email defined and return false', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // const user = findUserById(2);
+    // expect(xxxxx).xxxxx(); // ตรวจสอบว่าผู้ใช้ไม่มีอีเมล
   });
 
   it('should find a name in an array', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // const names = ["John Doe", "Jane Doe"];
+    // const nameToFind = "Jane Doe";
+
+    // // ตรวจสอบว่าชื่อที่ต้องการค้นหามีอยู่ในอาร์เรย์
+    // expect(findNameInArray(names, nameToFind)).xxxxx(nameToFind);
   });
 });
