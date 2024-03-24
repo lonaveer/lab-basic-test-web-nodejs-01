33fd2d63fbee883a658039f90ef38fa464f8000f
Jaroon Umpornpoolsuk <jaroon_ump_fth@tdem.toyota-asia.com>
2024-03-24 18:38:41 +0700

    lab01-question-04v2

--- src/test/unit-test/utils/userManager2.test.ts
+++ src/test/unit-test/utils/userManager2.test.ts
@@ -1,17 +1,35 @@
 import { createUser, findUserById, deleteUser } from '../../../app/utils/userManager2';
 
 describe('User Management Tests', () => {
-  // ใช้ beforeEach สำหรับการเพิ่มข้อมูลผู้ใช้งานทดสอบลงในฐานข้อมูลจำลองก่อนแต่ละการทดสอบ
-  // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+  // // ใช้ beforeEach สำหรับการเพิ่มข้อมูลผู้ใช้งานทดสอบลงในฐานข้อมูลจำลองก่อนแต่ละการทดสอบ
+  // beforeEach(() => {
+  //   xxxxx({ id: "1", name: "John Doe", email: "john@example.com" });
+  // });
 
-  // ใช้ afterEach สำหรับการลบข้อมูลผู้ใช้งานออกจากฐานข้อมูลจำลองหลังจากแต่ละการทดสอบ เพื่อไม่ให้ข้อมูลการทดสอบซ้ำซ้อน
-  // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+  // // ใช้ afterEach สำหรับการลบข้อมูลผู้ใช้งานออกจากฐานข้อมูลจำลองหลังจากแต่ละการทดสอบ เพื่อไม่ให้ข้อมูลการทดสอบซ้ำซ้อน
+  // afterEach(() => {
+  //   xxxxx("1");
+  // });
 
   it('should find a user by id', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // find a user "1"
+    // const user = xxxxx("1");
+
+    // found user should be defined
+    // expect(xxxxx).xxxxx();
+
+    // found user name should be "John Doe"
+    // expect(xxxxx?.name).xxxxx("John Doe");
   });
 
   it('should delete a user by id', () => {
-    // ..... กรุณาเขียนโค้ดคำตอบลงในนี้ ..... //
+    // // delete a user "1"
+    // xxxxx("1");
+
+    // // find a user "1"
+    // const user = xxxxx("1");
+
+    // // found user should be undefined
+    // expect(xxxxx).xxxxx();
   });
 });
